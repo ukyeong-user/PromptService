f55d2263805034033846d0701131f1b2b9b55839
ukyeong-user <hyk040520@naver.com>
2025-10-20 20:43:36 +0900

    chore: 나머지

---
+++ Desktop/멋사/겨울잠/PromptStudio/src/features/Main/Archive/Archive.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Archive() {
+  return <div>Archive 페이지</div>;
+}
---
+++ Desktop/멋사/겨울잠/PromptStudio/src/features/Main/Hub/Hub.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Hub() {
+  return <div>Hub 페이지</div>;
+}
