7c867ee8f8cee57379df79ea6c99d229e1998bfa
ukyeong-user <hyk040520@naver.com>
2025-10-23 19:44:31 +0900

    feat: SidePanel 닫기 버튼 위치 변경

--- src/pages/MakerPage/SidePanel/SidePanel.jsx
+++ src/pages/MakerPage/SidePanel/SidePanel.jsx
@@ -6,11 +6,8 @@ import PromptHublist from "./PromptHublist";
 function SidePanel({ onToggle }) {
   return (
     <SidebarWrapper>
-      <TopPanel />
+      <TopPanel onClose={onToggle} />
       <PromptHublist />
-      <CloseButton onClick={onToggle} aria-label="사이드바 닫기">
-        <CloseIcon>«</CloseIcon>
-      </CloseButton>
     </SidebarWrapper>
   );
 }
@@ -25,30 +22,4 @@ const SidebarWrapper = styled.div`
   flex-direction: column;
   position: relative;
   box-shadow: 5px 0px 26px 0px rgba(0, 0, 0, 0.06);
-`;
-
-const CloseButton = styled.button`
-  position: absolute;
-  left: calc(100% - 13px);
-  top: 50%;
-  transform: translateY(-50%);
-  width: 13px;
-  height: 120px;
-  background-color: #d9d9d9;
-  border: none;
-  border-radius: 0 6px 6px 0;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &:hover {
-    background-color: #c0c0c0;
-  }
-`;
-
-const CloseIcon = styled.span`
-  color: #666;
-  font-size: 1.5rem;
-  font-weight: bold;
 `;
--- src/pages/MakerPage/SidePanel/TopPanel.jsx
+++ src/pages/MakerPage/SidePanel/TopPanel.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import styled from "styled-components";
 import SearchInput from "../../../components/SearchInput";
 import HubModalSelector from "../components/HubModalSelector";
+import SidePanelClose from "../../../assets/icon/tabler_chevrons-open.svg";
 
-function TopPanel() {
+function TopPanel({ onClose }) {
   return (
     <TopContainer>
       <SearchInput />
       <HubModalSelector />
+      {onClose && (
+        <CloseButton onClick={onClose} aria-label="사이드바 닫기">
+          <CloseIcon src={SidePanelClose} />
+        </CloseButton>
+      )}
     </TopContainer>
   );
 }
@@ -25,4 +31,28 @@ const TopContainer = styled.div`
   padding: 2.7vh 1.93vw;
   width: 100%;
   background-color: #001e40;
+  position: relative;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
+  width: 2rem;
+  height: 2rem;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const CloseIcon = styled.img`
+  width: 10vh;
+  height: auto;
 `;
